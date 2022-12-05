1de70acdc795164403efbb6bda272360acfa1c29
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-12-05 05:04:44 -0600

    drawing on the screen

--- src/TbdOfSPLORR/Root.vb
+++ src/TbdOfSPLORR/Root.vb
@@ -2,6 +2,19 @@
     Inherits Game
     Private graphics As GraphicsDeviceManager
     Private spriteBatch As SpriteBatch
+    Private texture As Texture2D
+    Private sourceRectangles As Rectangle()
+    Private Const CellWidth = 16
+    Private Const CellHeight = 24
+    Private destinationRectangles As Rectangle()
+    Private Const CellColumns = 32
+    Private Const CellRows = 16
+    Private Const ViewWidth = CellColumns * CellWidth
+    Private Const ViewHeight = CellRows * CellHeight
+    Private Const ScreenWidth = 640
+    Private Const ScreenHeight = 480
+    Private Const OffsetX = (ScreenWidth - ViewWidth) \ 2
+    Private Const OffsetY = (ScreenHeight - ViewHeight) \ 2
     Sub New()
         graphics = New GraphicsDeviceManager(Me)
         graphics.PreferredBackBufferWidth = 640
@@ -15,6 +28,21 @@
     Protected Overrides Sub LoadContent()
         MyBase.LoadContent()
         spriteBatch = New SpriteBatch(GraphicsDevice)
+        texture = Texture2D.FromFile(GraphicsDevice, "Content/CoCoFont.png")
+        Dim rows = texture.Height \ CellHeight
+        Dim columns = texture.Width \ CellWidth
+        ReDim sourceRectangles(rows * columns)
+        For row = 0 To rows - 1
+            For column = 0 To columns - 1
+                sourceRectangles(row * columns + column) = New Rectangle(column * CellWidth, row * CellHeight, CellWidth, CellHeight)
+            Next
+        Next
+        ReDim destinationRectangles(CellColumns * CellRows)
+        For row = 0 To CellRows - 1
+            For column = 0 To CellColumns - 1
+                destinationRectangles(column + row * CellColumns) = New Rectangle(column * CellWidth + OffsetX, row * CellHeight + OffsetY, CellWidth, CellHeight)
+            Next
+        Next
     End Sub
     Protected Overrides Sub Update(gameTime As GameTime)
         If Keyboard.GetState().IsKeyDown(Keys.Escape) Then
@@ -23,7 +51,12 @@
         MyBase.Update(gameTime)
     End Sub
     Protected Overrides Sub Draw(gameTime As GameTime)
-        GraphicsDevice.Clear(Color.CornflowerBlue)
+        GraphicsDevice.Clear(Color.Black)
+        spriteBatch.Begin()
+        For index = 0 To destinationRectangles.Length - 1
+            spriteBatch.Draw(texture, destinationRectangles(index), sourceRectangles(255), Color.White)
+        Next
+        spriteBatch.End()
         MyBase.Draw(gameTime)
     End Sub
 End Class
