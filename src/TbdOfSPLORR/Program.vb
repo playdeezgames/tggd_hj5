4008abf3659feed9468b0e6f3708f0d8d757ea82
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-12-05 04:42:46 -0600

    got a monogame window

---
+++ src/TbdOfSPLORR/Program.vb
@@ -0,0 +1,7 @@
+Module Program
+    Sub Main(args As String())
+        Using root As New Root()
+            root.Run()
+        End Using
+    End Sub
+End Module
---
+++ src/TbdOfSPLORR/Root.vb
@@ -0,0 +1,29 @@
+﻿Public Class Root
+    Inherits Game
+    Private graphics As GraphicsDeviceManager
+    Private spriteBatch As SpriteBatch
+    Sub New()
+        graphics = New GraphicsDeviceManager(Me)
+        graphics.PreferredBackBufferWidth = 640
+        graphics.PreferredBackBufferHeight = 480
+        graphics.ApplyChanges()
+        Content.RootDirectory = "Content"
+    End Sub
+    Protected Overrides Sub Initialize()
+        MyBase.Initialize()
+    End Sub
+    Protected Overrides Sub LoadContent()
+        MyBase.LoadContent()
+        spriteBatch = New SpriteBatch(GraphicsDevice)
+    End Sub
+    Protected Overrides Sub Update(gameTime As GameTime)
+        If Keyboard.GetState().IsKeyDown(Keys.Escape) Then
+            [Exit]()
+        End If
+        MyBase.Update(gameTime)
+    End Sub
+    Protected Overrides Sub Draw(gameTime As GameTime)
+        GraphicsDevice.Clear(Color.CornflowerBlue)
+        MyBase.Draw(gameTime)
+    End Sub
+End Class
