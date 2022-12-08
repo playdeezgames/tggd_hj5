f697483f1d5ebad57af972927bdba05970baac7f
TheGrumpyGameDev <ernestpazera@gmail.com>
2022-12-08 05:03:17 -0600

    shipped it!

--- shippit-tbd.sh
+++ shippit-tbd.sh
@@ -1,6 +1,6 @@
 dotnet publish ./src/TbdOfSPLORR/TbdOfSPLORR.vbproj -o ./pub-linux-tbd -c Release --sc -r linux-x64
 dotnet publish ./src/TbdOfSPLORR/TbdOfSPLORR.vbproj -o ./pub-windows-tbd -c Release --sc -r win-x64
-butler push pub-windows thegrumpygamedev/tbd-of-splorr:windows
-butler push pub-linux thegrumpygamedev/tbd-of-splorr:linux
+butler push pub-windows-tbd thegrumpygamedev/tbd-of-splorr:windows
+butler push pub-linux-tbd thegrumpygamedev/tbd-of-splorr:linux
 git add -A
 git commit -m "shipped it!"
