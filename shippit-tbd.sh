44a8ef7b542e9c5f38764918873ec30c4c868a35
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-09-08 04:45:05 -0500

    shipped it!

--- shippit-tbd.sh
+++ shippit-tbd.sh
@@ -1,6 +1,11 @@
-dotnet publish ./src/TbdOfSPLORR/TbdOfSPLORR.vbproj -o ./pub-linux-tbd -c Release --sc -r linux-x64
-dotnet publish ./src/TbdOfSPLORR/TbdOfSPLORR.vbproj -o ./pub-windows-tbd -c Release --sc -r win-x64
+rm -rf ./pub-linux-tbd
+rm -rf ./pub-windows-tbd
+rm -rf ./pub-mac-tbd
+dotnet publish ./src/TbdOfSPLORR/TbdOfSPLORR.vbproj -o ./pub-linux-tbd -c Release -p:PublishSingleFile=true --sc -r linux-x64
+dotnet publish ./src/TbdOfSPLORR/TbdOfSPLORR.vbproj -o ./pub-windows-tbd -c Release -p:PublishSingleFile=true --sc -r win-x64
+dotnet publish ./src/TbdOfSPLORR/TbdOfSPLORR.vbproj -o ./pub-mac-tbd -c Release -p:PublishSingleFile=true --sc -r osx-x64
 butler push pub-windows-tbd thegrumpygamedev/tbd-of-splorr:windows
 butler push pub-linux-tbd thegrumpygamedev/tbd-of-splorr:linux
+butler push pub-mac-tbd thegrumpygamedev/tbd-of-splorr:mac
 git add -A
 git commit -m "shipped it!"
