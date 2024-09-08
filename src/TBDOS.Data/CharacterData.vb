2fb5bed9f67d14b4224971ea398e179a3320069f
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-09-08 05:05:08 -0500

    new project with data layer copy.

---
+++ src/TBDOS.Data/CharacterData.vb
@@ -0,0 +1,7 @@
+﻿Public Class CharacterData
+    Public Property LocationId As Integer
+    Public Property Direction As Integer
+    Public Property Messages As List(Of String())
+    Public Property Statistics As Dictionary(Of Integer, Integer)
+    Public Property Items As Dictionary(Of Integer, Integer)
+End Class
---
+++ src/TBDOS.Data/WorldData.vb
@@ -0,0 +1,5 @@
+﻿Public Class WorldData
+    Public Property PlayerCharacterId As Integer
+    Public Property Characters As List(Of CharacterData)
+    Public Property Locations As List(Of LocationData)
+End Class
