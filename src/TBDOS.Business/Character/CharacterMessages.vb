6dacfad70327f867538683df8f5e6942c5762259
TheGrumpyGameDev <ernestpazera@gmail.com>
2024-09-09 04:51:52 -0500

    character messages moved

--- src/TBDOS.Business/Character/CharacterMessages.vb
+++ src/TBDOS.Business/Character/CharacterMessages.vb
@@ -5,4 +5,32 @@
     Public Sub New(worldData As Data.WorldData, characterId As Integer)
         MyBase.New(worldData, characterId)
     End Sub
+
+    Public ReadOnly Property NextMessage As IEnumerable(Of String) Implements ICharacterMessages.NextMessage
+        Get
+            If Not HasMessages Then
+                Return Array.Empty(Of String)
+            End If
+            Return WorldData.Characters(CharacterId).Messages.First
+        End Get
+    End Property
+
+    Public ReadOnly Property HasMessages As Boolean Implements ICharacterMessages.HasMessages
+        Get
+            Return WorldData.Characters(CharacterId).Messages.Any
+        End Get
+    End Property
+
+    Public Sub AddMessage(ParamArray lines() As String) Implements ICharacterMessages.AddMessage
+        If CharacterId <> WorldData.PlayerCharacterId Then
+            Return
+        End If
+        WorldData.Characters(CharacterId).Messages.Add(lines)
+    End Sub
+
+    Public Sub DismissMessage() Implements ICharacterMessages.DismissMessage
+        If HasMessages Then
+            WorldData.Characters(CharacterId).Messages.RemoveAt(0)
+        End If
+    End Sub
 End Class
--- src/TBDOS.Business/Character/ICharacterMessages.vb
+++ src/TBDOS.Business/Character/ICharacterMessages.vb
@@ -1,2 +1,6 @@
 ﻿Public Interface ICharacterMessages
+    ReadOnly Property NextMessage As IEnumerable(Of String)
+    Sub AddMessage(ParamArray lines As String())
+    ReadOnly Property HasMessages As Boolean
+    Sub DismissMessage()
 End Interface
